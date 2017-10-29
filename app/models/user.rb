46e13ab2bc17344ba3dfd2bed476522fed4dd698
Angelina sisi xia <axia@sfu.ca>
2017-10-29 13:36:25 -0700

    added password confirmation, and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	validates :username, :email, :password_digest, :presence => true
+	validates :username, uniqueness: true
+	validates :email, uniqueness: true
+  validates_confirmation_of :password
+	validates :password_confirmation, presence: true
+	VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+	                  format: { with: VALID_REGEX },
+	                  uniqueness: { case_sensitive: false }
+
 end
