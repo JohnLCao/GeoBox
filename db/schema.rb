61fc5b4a2c86c80f44a24f0501ab9116096c58d4
Brendin Green <brending@sfu.ca>
2017-11-01 08:32:28 -0700

    Document model complete with belongs_to and has_many. Also some edits to get basic creation going.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023130843) do
+ActiveRecord::Schema.define(version: 20171101145604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "documents", force: :cascade do |t|
+    t.string   "filename"
+    t.text     "description"
+    t.string   "latitude"
+    t.string   "longitude"
+    t.string   "cloudKey"
+    t.integer  "user"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
