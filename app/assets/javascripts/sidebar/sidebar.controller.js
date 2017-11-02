c80fc46d037fa9e132b0ed5487060d170cdd49cc
John L. Cao <john_caohaha@hotmail.com>
2017-11-02 13:19:50 -0700

    wrap sidebar component in angular(consider moving jquery done oninit code into angular), dynamically show logout, home, new document navbar options only if user is logged in

---
+++ app/assets/javascripts/sidebar/sidebar.controller.js
@@ -0,0 +1,17 @@
+(function(){
+
+'use strict';
+
+angular.module('GeoBox')
+.controller('SidebarController', SidebarController);
+
+SidebarController.$inject = ['$http', '$rootScope']; 
+function SidebarController($http, $rootScope){
+	var $ctrl = this;
+
+	$ctrl.$onInit = function(){
+		$rootScope.$broadcast('login:login', {}); // we can consider adding data to this event
+	}
+}
+
+})();
