2f3d15a5b9b9065fd4b174adf492ed1e3a885b68
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-11-01 17:50:44 -0700

    updated uses_controller to fetch documents through document helper module. in module mutual radius is implemented but not yet tested. will require refactoring

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,18 @@
 module DocumentsHelper
+  mockRadius = 0.00001497678
+  setRadius = mockRadius
+
+  def fetchfiles(Latlng)
+    Document.all.select{|doc| mutualRadius?(Latlng, doc.latitude, doc.longitude)}
+  end
+
+  def mutualRadius?(userCurrentLatlng, doc_lat, doc_lng)
+    lat = userCurrentLatlng.split(',')[0].to_i
+    lng = userCurrentLatlng.split(',')[1].to_i
+    deltaLat = lat - doc_lat.to_i
+    deltaLng = lng - doc_lng.to_i
+    distanceInDeg = (deltaLat * deltaLat + deltaLng * deltaLng) ** 0.5 #pythag hypotenuse
+    return (distanceInDeg < setRadius)
+  end
+
 end
