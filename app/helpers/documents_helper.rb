919cc4d9ebee601d44e29b2cf4714b22c6b9775f
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-11-02 11:00:03 -0700

    rename latlng in dochelper, john please review and merge if necessary

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -7,10 +7,10 @@ module DocumentsHelper
   end
 
   def mutualRadius?(userCurrentLatlng, doc_lat, doc_lng)
-    lat = userCurrentLatlng.split(',')[0].to_i
-    lng = userCurrentLatlng.split(',')[1].to_i
-    deltaLat = lat - doc_lat.to_i
-    deltaLng = lng - doc_lng.to_i
+    userlat = userCurrentLatlng.split(',')[0].to_i
+    userlng = userCurrentLatlng.split(',')[1].to_i
+    deltaLat = userlat - doc_lat.to_i
+    deltaLng = userlng - doc_lng.to_i
     distanceInDeg = (deltaLat * deltaLat + deltaLng * deltaLng) ** 0.5 #pythag hypotenuse
     return (distanceInDeg < setRadius)
   end
