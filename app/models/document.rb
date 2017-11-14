03dcd2911d12451aa724b02e8809ee90872b6fd8
John L. Cao <john_caohaha@hotmail.com>
2017-11-14 12:02:39 -0800

    allow dup files for different users, but not same user

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class Document < ActiveRecord::Base
   belongs_to :user
-  validates :filename, uniqueness: true
+  validates_uniqueness_of :filename, scope: :user 
   validates :filename, :attachment, :user, :description, :latitude, :longitude, presence: true
   mount_uploader :attachment, AttachmentUploader
 end
