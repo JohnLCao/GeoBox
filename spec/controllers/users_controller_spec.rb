5d581123a265f8effe721b3d30d8c2ccdd138a24
Tu Dat Nguyen <you@example.com>
2017-11-14 20:17:38 -0800

    rspec: add unit tests: #index, #show, #update for users_controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,17 +1,71 @@
 require "rails_helper"
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
   describe "#index" do
-    it "responds successfully for admin" do
-      login_as_admin
-      get :index
-      expect(response).to be_success
+    context "as an admin user" do
+      it "responds successfully" do
+        login_as_admin
+        get :index
+        expect(response).to be_success
+      end
     end
 
-    it "responds unsucessfully for regular user" do
-      login(FactoryBot.create(:user))
-      get :index
-      expect(response).to_not be_success
+    context "as not an admin user" do
+      it "responds unsucessfully for regular user" do
+        login(user)
+        get :index
+        expect(response).to_not be_success
+      end
+
+      it "responds unsucessfully for guest" do
+        get :index
+        expect(response).to_not be_success
+      end
+
+      it "redirects to the login page" do
+        get :index
+        expect(response).to redirect_to login_path
+      end
     end
   end
+
+  describe "#show" do
+    context "as an authorized user" do
+      it "responds successfully" do
+        login(user)
+        get :show, id: user.id
+        expect(response).to be_success
+      end
+    end
+
+    context "as an unauthorized user" do
+      it "redirects to login page" do
+        get :show, id: ""
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
+
+  describe "#update" do
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+      it "responds successfully" do
+        user_params = FactoryBot.attributes_for(:user, username: "new_user_name")
+        login(@user)
+        patch :update, id: @user.id, user: user_params
+        expect(@user.reload.username).to eq "new_user_name"
+      end
+    end
+
+    # context "as an unauthorized user" do
+    #   it "redirects to login page" do
+    #     get :show, id: ""
+    #     expect(response).to redirect_to login_path
+    #   end
+    # end
+  end
+
 end
--- spec/features/upload_file_spec.rb
+++ spec/features/upload_file_spec.rb
@@ -3,17 +3,33 @@ require 'rails_helper'
 RSpec.feature "UploadFile", type: :feature, js: true do
   # pending "add some scenarios (or delete) #{__FILE__}"
   let(:admin) { FactoryBot.create(:admin) }
-  scenario "admin uploads a file" do
+  let(:user) { FactoryBot.create(:user) }
+  # scenario "admin uploads a file" do
+  #   visit login_path
+  #   fill_in "Username", with: admin.username
+  #   fill_in "Password", with: admin.password
+  #   click_button "Login"
+  #   click_on "Documents"
+  #   click_on "New Document"
+  #   fill_in "Description", with: "Just for fun"
+  #   attach_file "Attachment", "#{Rails.root}/spec/files/test.txt"
+  #   click_button "Submit"
+  #   expect(page).to have_content 'Document was successfully created.'
+  #   expect(page).to have_current_path root_path
+  # end
+
+  scenario "user uploads a file" do
     visit login_path
-    fill_in "Username", with: admin.username
-    fill_in "Password", with: admin.password
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
     click_button "Login"
-    click_on "Documents"
-    click_on "New Document"
+    # manual call upload Document
+    visit new_document_path
     fill_in "Description", with: "Just for fun"
     attach_file "Attachment", "#{Rails.root}/spec/files/test.txt"
     click_button "Submit"
     expect(page).to have_content 'Document was successfully created.'
     expect(page).to have_current_path root_path
+    sleep(5)
   end
 end
