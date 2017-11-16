362a81add2afc75d92c7b60d695faf28c8ef276a
Tu Dat Nguyen <you@example.com>
2017-11-15 21:46:39 -0800

    rspec: #index documents_controller

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe DocumentsController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
   describe "#index" do
     context "as an admin user" do
       it "responds successfully" do
@@ -9,5 +10,25 @@ RSpec.describe DocumentsController, type: :controller do
         expect(response).to be_success
       end
     end
+
+    # it should respond successfully to index page for the owner
+    context "as not an admin user" do
+      it "responds unsucessfully for regular user" do
+        login(user)
+        get :index
+        expect(response).to_not be_success
+      end
+
+      it "responds unsucessfully for guest" do
+        get :index
+        expect(response).to_not be_success
+      end
+
+      it "redirects to the login page" do
+        get :index
+        expect(response).to redirect_to login_path
+      end
+    end
   end
+
 end
