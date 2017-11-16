59681d570926127d90077c8e855115ea91b41a9b
Tu Dat Nguyen <you@example.com>
2017-11-15 22:02:24 -0800

    rspec: #show documents_controller

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe DocumentsController, type: :controller do
-  let(:user) { FactoryBot.create(:user) }
   describe "#index" do
+    let(:user) { FactoryBot.create(:user) }
     context "as an admin user" do
       it "responds successfully" do
         login_as_admin
@@ -26,6 +26,36 @@ RSpec.describe DocumentsController, type: :controller do
 
       it "redirects to the login page" do
         get :index
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
+
+  describe "#show" do
+    before do
+      @user1 = FactoryBot.create(:user)
+      @user2 = FactoryBot.create(:user)
+      @document = FactoryBot.create(:document, user_id: @user1.id)
+    end
+    context "as an authorized (owner) user" do
+      it "responds successfully" do
+        login(@user1)
+        get :show, id: @document.id
+        expect(response).to be_success
+      end
+    end
+
+    context "as an unauthorized user" do
+      it "responds unsucessfully for not the owner" do
+        login(@user2)
+        get :show, id: @document.id
+        expect(response).not_to be_success
+        expect(response).to redirect_to root_path
+        # will decide where it should redirect to
+      end
+
+      it "redirects to the login page for guest" do
+        get :show, id: @document.id
         expect(response).to redirect_to login_path
       end
     end
