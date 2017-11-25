42d27fdd558b1ca834b39d49bc00c3b261a90b91
John L. Cao <john_caohaha@hotmail.com>
2017-11-25 13:36:42 -0800

    migrations fixer upper

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171125200228) do
+ActiveRecord::Schema.define(version: 20171125213237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171125200228) do
     t.text     "description"
     t.string   "latitude"
     t.string   "longitude"
+    t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -32,6 +33,7 @@ ActiveRecord::Schema.define(version: 20171125200228) do
     t.string   "latitude"
     t.string   "longitude"
     t.integer  "user_id"
+    t.integer  "book_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "attachment"
