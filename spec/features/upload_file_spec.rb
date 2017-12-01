f727b2abd34fa33c23b2939837f6b06ce2cd1c1c
Tu Dat Nguyen <you@example.com>
2017-11-30 23:32:10 -0800

    rspec: integration test: upload a file without a book by regular users and admins

--- spec/features/upload_file_spec.rb
+++ spec/features/upload_file_spec.rb
@@ -4,27 +4,28 @@ RSpec.feature "UploadFile", type: :feature, js: true do
   # pending "add some scenarios (or delete) #{__FILE__}"
   let(:admin) { FactoryBot.create(:admin) }
   let(:user) { FactoryBot.create(:user) }
-  # scenario "admin uploads a file" do
-  #   visit login_path
-  #   fill_in "Username", with: admin.username
-  #   fill_in "Password", with: admin.password
-  #   click_button "Login"
-  #   click_on "Documents"
-  #   click_on "New Document"
-  #   fill_in "Description", with: "Just for fun"
-  #   attach_file "Attachment", "#{Rails.root}/spec/files/test.txt"
-  #   click_button "Submit"
-  #   expect(page).to have_content 'Document was successfully created.'
-  #   expect(page).to have_current_path root_path
-  # end
+  scenario "admin uploads a file without a book" do
+    visit login_path
+    fill_in "Username", with: admin.username
+    fill_in "Password", with: admin.password
+    click_button "Login"
+    click_on "Dashboard"
+    click_on "Documents"
+    click_on "New Document"
+    fill_in "Description", with: "Just for fun"
+    attach_file "Attachment", "#{Rails.root}/spec/files/test.txt"
+    click_button "Submit"
+    expect(page).to have_content 'Document was successfully created.'
+    expect(page).to have_current_path root_path
+  end
 
-  scenario "user uploads a file" do
+  scenario "user uploads a file without a book" do
     visit login_path
     fill_in "Username", with: user.username
     fill_in "Password", with: user.password
     click_button "Login"
-    sleep(7)
-    click_on(class: 'fa fa-plus btn')
+    find_by_id('dropdownMenuButton').click
+    click_on "Document"
     fill_in "Description", with: "Just for fun"
     attach_file "Attachment", "#{Rails.root}/spec/files/test2.txt"
     click_button "Submit"
