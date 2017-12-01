915ffe0b8b474943e6ccd7853c72af0a09fa59e1
Tu Dat Nguyen <you@example.com>
2017-12-01 00:10:33 -0800

    rspec: integration test: creat book and add file to book

--- spec/features/upload_file_spec.rb
+++ spec/features/upload_file_spec.rb
@@ -32,4 +32,33 @@ RSpec.feature "UploadFile", type: :feature, js: true do
     expect(page).to have_text 'Document was successfully created.'
     expect(page).to have_current_path root_path
   end
+
+  scenario "user create a book and upload a file" do
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+    click_button "Login"
+    find_by_id('dropdownMenuButton').click
+    click_on "Book"
+    expect(page).to have_current_path new_book_path
+    fill_in "Name", with: "Test book 1"
+    fill_in "Description", with: "Test creating a book"
+    fill_in "Key", with: "simple"
+    click_button "Submit"
+    expect(page).to have_text 'Book was successfully created.'
+    expect(page).to have_current_path root_path
+    find_link(href: '#books').click
+    click_on "View Contents"
+    fill_in "Key", with: "simple"
+    click_on "Add Document"
+    fill_in "Description", with: "Test add file to book"
+    attach_file "Attachment", "#{Rails.root}/spec/files/test2.txt"
+    click_button "Submit"
+    expect(page).to have_text 'Document was successfully created.'
+    expect(page).to have_current_path root_path
+    find_link(href: '#books').click
+    click_on "View Contents"
+    fill_in "Key", with: "simple"
+    expect(page).to have_content "Test book 1"
+  end
 end
