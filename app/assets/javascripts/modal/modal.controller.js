357191d711064395582b3d41e05e4bb333343e2c
Brendin <brending@sfu.ca>
2017-12-01 11:56:35 -0800

    Fuzzy search (Part 2).

--- app/assets/javascripts/modal/modal.controller.js
+++ app/assets/javascripts/modal/modal.controller.js
@@ -11,6 +11,34 @@ function ModalController($rootScope, DocumentService, $scope, UserService){
     $ctrl.docs = [];
     $ctrl.book = {};
 
+    $ctrl.fuzzySearchString = '';
+
+    $ctrl.fuzzySearchList = [];
+
+    $ctrl.fuzzySearchOptions = {
+        shouldSort: true,
+        threshold: 0.6,
+        location: 0,
+        distance: 30,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+            "name",
+            "description",
+            "username"
+        ]
+    };
+
+    $ctrl.fuzzySearchResult = [];
+
+    $ctrl.search = function(){
+        if ($ctrl.fuzzySearchString){
+            $ctrl.fuzzySearchResult = $ctrl.fuzzySearchList.search($ctrl.fuzzySearchString);
+        } else {
+            $ctrl.fuzzySearchResult = $ctrl.docs; // all documents.
+        }
+    };
+
     $scope.$on('files_in_book:ready', function(event, data){
         $("#exampleModal").modal("show");
         $ctrl.allow_access = false;
@@ -24,18 +52,18 @@ function ModalController($rootScope, DocumentService, $scope, UserService){
         if ($ctrl.secret === $ctrl.book.key){
             $ctrl.allow_access = true;
         }
-    }
+    };
 
     $ctrl.getDocs = function(book_id){
-        console.log(book_id);
         $ctrl.docs = processDocuments(DocumentService.files_in_book);
-        console.log($ctrl.docs);
     };
 
     function processDocuments(documents){
         documents.forEach(function(d){
             d.created_at = moment(new Date(d.created_at_ms * 1000)).fromNow();
         });
+        $ctrl.fuzzySearchList = new Fuse(documents, $ctrl.fuzzySearchOptions);
+        $ctrl.fuzzySearchResult = documents;
         return documents;
     }
 
