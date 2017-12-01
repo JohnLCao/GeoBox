ea4991a7fcec49719cb3c3215db12670f8f77144
Tu Dat Nguyen <you@example.com>
2017-12-01 00:51:24 -0800

    rspec: integration test: search for file/ book; UI test

--- spec/features/app_flow_spec.rb
+++ spec/features/app_flow_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "AppFlow", type: :feature, js: true do
   # pending "add some scenarios (or delete) #{__FILE__}"
   let(:user) { FactoryBot.create(:user) }
   # self.use_transactional_fixtures = false
-  scenario "user login - view files - view profile - logout" do
+  scenario "user login - view files - view books - view profile - logout" do
     # user = FactoryBot.create(:user)
     visit login_path
     fill_in "Username", with: user.username
@@ -13,6 +13,8 @@ RSpec.feature "AppFlow", type: :feature, js: true do
     expect(page).to have_current_path root_path
     find_link(href: "#home").click
     expect(page).to have_content "Files"
+    find_link(href: "#books").click
+    expect(page).to have_content "Books"
     find_link(href: "#profile").click
     expect(page).to have_content user.username
     expect(page).to have_content user.email
--- spec/features/download_file_spec.rb
+++ spec/features/download_file_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "DownloadFile", type: :feature, js: true do
   # pending "add some scenarios (or delete) #{__FILE__}"
   let(:user) { FactoryBot.create(:user) }
-  scenario "user login - upload files - view files - download - logout" do
+  scenario "user login - upload files - view files - search for a file - download - logout" do
     visit login_path
     fill_in "Username", with: user.username
     fill_in "Password", with: user.password
@@ -14,13 +14,18 @@ RSpec.feature "DownloadFile", type: :feature, js: true do
     attach_file "Attachment", "#{Rails.root}/spec/files/test2.txt"
     click_button "Submit"
     find_link(href: '#home').click
+    find_by_id('search').click
+    fill_in "Search for files...", with: "for sure you can't find it"
+    expect(page).not_to have_content "test2.txt"
+    fill_in "Search for files...", with: "tes"
+    expect(page).to have_content "test2.txt"
     click_on("test2.txt")
     expect(page).to have_content "This is to test the download file function."
     page.driver.go_back
     click_on "Logout"
   end
 
-  scenario "user login - create and add file to the book - view books - download - logout" do
+  scenario "user login - create and add file to the book - view books - search for a book - download - logout" do
     visit login_path
     fill_in "Username", with: user.username
     fill_in "Password", with: user.password
@@ -40,6 +45,10 @@ RSpec.feature "DownloadFile", type: :feature, js: true do
     attach_file "Attachment", "#{Rails.root}/spec/files/test2.txt"
     click_button "Submit"
     find_link(href: '#books').click
+    fill_in "Search for books...", with: "for sure you can't find it"
+    expect(page).not_to have_content "Book 1"
+    fill_in "Search for books...", with: "ook 1"
+    expect(page).to have_content "Book 1"
     click_on "View Contents"
     fill_in "Key", with: "simple"
     click_on("test2.txt")
