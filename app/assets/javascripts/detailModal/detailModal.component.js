83d344371b60ca1354d2e801adfd616c3ab5333e
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-12-22 10:52:00 -0800

    WIP added modal controllers, compoent, and templates

---
+++ app/assets/javascripts/detailModal/detailModal.component.js
@@ -0,0 +1,11 @@
+(function(){
+
+'use strict';
+
+angular.module('GeoBox')
+.component('geoboxDetailModal', {
+    templateUrl: 'detailModal.html',
+    controller: 'DetailModalController'
+});
+
+})();
