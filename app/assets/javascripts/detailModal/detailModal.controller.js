9cdc48937d43bd52bd3260ae7814cd5baf59833e
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-12-22 10:52:00 -0800

    WIP added modal controllers, compoent, and templates

---
+++ app/assets/javascripts/detailModal/detailModal.controller.js
@@ -0,0 +1,11 @@
+(function() {
+'use strict';
+
+angular.module('GeoBox')
+.controller('DetailModalController', DetailModalController);
+
+DetailModalController.$inject = ['$rootScope', 'DocumentService', '$scope', 'UserService'];
+
+function DetailModalController($rootScope, DocumentService, $scope, UserService){
+    // deisplay doc in detail modal view
+})();
