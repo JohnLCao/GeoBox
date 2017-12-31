7317f8e398f265907a0d26484310742a39a545d9
John L. Cao <john_caohaha@hotmail.com>
2017-12-30 16:24:54 -0800

    add username to chat messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230201459) do
+ActiveRecord::Schema.define(version: 20171230232522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20171230201459) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "room_id"
+    t.string   "username"
   end
 
   create_table "rooms", force: :cascade do |t|
